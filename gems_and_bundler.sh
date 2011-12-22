64f240892f6689abb7e4d4a5a52a768026994092
patrickgombert <patrickgombert@gmail.com>
2011-12-22 10:48:18 -0600

    Edit PATH

--- gems_and_bundler.sh
+++ gems_and_bundler.sh
@@ -2,5 +2,5 @@ mkdir ~/rubygems
 cd ~/rubygems && wget http://rubyforge.org/frs/download.php/75573/rubygems-1.8.12.tgz
 tar -zxvf rubygems-1.8.12.tgz
 cd ./rubygems-1.8.12
-sudo ruby setup.rb
-sudo gem install bundler
+ruby setup.rb
+gem install bundler
--- ruby.sh
+++ ruby.sh
@@ -1,7 +1,8 @@
-mkdir ~/.rbenv/versions/1.9.2-p290
-cd ~/.rbenv/versions/1.9.2-p290 && wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.bz2
+cd ~/.rbenv/versions/ && wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p290.tar.bz2
 tar -jxvf ruby-1.9.2-p290.tar.bz2
-./configure --prefix=$HOME/.rbenv/versions/1.9.2-p290
+rm ruby-1.9.2-p290.tar.bz2
+mv ruby-1.9.2-p290 1.9.2-p290
+cd ./1.9.2-p290 && ./configure --prefix=$HOME/.rbenv/versions/1.9.2-p290
 make
 make install
 rbenv rehash
