5ba4a0d2e5fc10fc004245c4e327cafba8108ffe
Alex Varju <varju@blackboard.com>
2012-03-04 00:09:07 -0800

    rename desktop file, add missing Type attribute

--- desktop.sh
+++ desktop.sh
@@ -1,11 +1,11 @@
-touch $HOME/Desktop/KidsRuby
-LOC=$HOME/Desktop/KidsRuby
+LOC=$HOME/Desktop/KidsRuby.desktop
 echo '[Desktop Entry]' > $LOC
 echo 'Version=1.0.5' >> $LOC
 echo 'Terminal=false' >> $LOC
+echo 'Type=Application' >> $LOC
 LINE1='Exec=bash '
 LINE2='/kidsruby/kidsruby_runner.sh'
 echo $LINE1$HOME$LINE2 >> $LOC
 echo 'Name=KidsRuby' >> $LOC
-sudo chmod +x $HOME/Desktop/KidsRuby
+sudo chmod +x $LOC
 cp -p ./kidsruby_runner.sh $HOME/kidsruby/kidsruby_runner.sh
