17227856f83da7bf0a2030f58f0258595baab5f0
Alex Varju <varju@blackboard.com>
2012-03-04 00:05:49 -0800

    put the runner script in the kidsruby directory, along with all the other files; copy it rather than moving so that the git source doesn't change

--- desktop.sh
+++ desktop.sh
@@ -4,8 +4,8 @@ echo '[Desktop Entry]' > $LOC
 echo 'Version=1.0.5' >> $LOC
 echo 'Terminal=false' >> $LOC
 LINE1='Exec=bash '
-LINE2='/kidsruby_runner.sh'
+LINE2='/kidsruby/kidsruby_runner.sh'
 echo $LINE1$HOME$LINE2 >> $LOC
 echo 'Name=KidsRuby' >> $LOC
 sudo chmod +x $HOME/Desktop/KidsRuby
-mv ./kidsruby_runner.sh $HOME/kidsruby_runner.sh
+cp -p ./kidsruby_runner.sh $HOME/kidsruby/kidsruby_runner.sh
